a45f059beb0a3947c5a1d7129be2136009529ee2
naman <naman0105@gmail.com>
2020-01-24 10:55:10 +0530

    some fun

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(1280,720);
+  // put setup code here
+}
+
+
+//getting random colorful dots and 
+// function draw() {
+//   if(mouseIsPressed){
+//     fill(192)
+//   } else{
+//     fill(Math.random()*255,Math.random()*255,Math.random()*255)
+//   }
+//   var x = Math.random()*1000;
+//   var y = Math.random()*1000;
+//   ellipse(x,y, 20,20)
+//   // put drawing code here
+// }
+
+function draw() {
+  noStroke()
+  if(mouseIsPressed){
+    fill(192)
+    ellipse(mouseX,mouseY, 20,20)
+  } else{
+    fill(Math.random()*255,Math.random()*255,Math.random()*255)
+    var x = Math.random()*1000;
+    var y = Math.random()*1000;
+    ellipse(x,y, 20,20)
+  }
+
+  // put drawing code here
+}
